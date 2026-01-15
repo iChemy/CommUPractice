ee88239abf15fb5c23d1e88ba434cdd594902283
iChemy <controlyuu.dev@gmail.com>
2026-01-15 23:36:24 +0900

    :construction: WIP

--- src/main/CCommUPosePractice.java
+++ src/main/CCommUPosePractice.java
@@ -34,6 +34,9 @@ public class CCommUPosePractice {
         pose.addServoAngle(Byte.valueOf(CCommUMotion.SV_R_SHOULDER_P), (short) 1000);
         CRobotPoseDebugger.cRobotPosePrint(pose);
 
+        // サーボモータのソフトスタート (これがないと動かない...?)
+        motion.ServoOn();
+
         CRobotUtil.Log(TAG, "play:" + motion.play(pose, 2000));
         motion.waitEndinterpAll();
         CRobotUtil.Log(TAG, "interpolation end");
